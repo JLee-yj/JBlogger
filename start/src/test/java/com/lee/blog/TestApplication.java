54d3c0f5fd367537aa46080aa4db39d80829839a
Liyuanjin <1421469216@qq.com>
2022-09-26 15:04:33 +0800

    优化包结构

--- start/src/test/java/com/lee/blog/TestApplication.java
+++ start/src/test/java/com/lee/blog/TestApplication.java
@@ -1,18 +1,11 @@
 package com.lee.blog;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
-@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = {TestApplication.class})})
-@SpringBootApplication(scanBasePackages = {"com.alibaba.craftsman"})
 public class TestApplication {
 
     public static void main(String[] args) {
-        //这里填的是TestApplication
-        ApplicationContext context = SpringApplication.run(TestApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
